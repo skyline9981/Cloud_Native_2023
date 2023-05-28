2799f8990f22c72124ebb5d1867c85b29238a547
skyline9981 <super890831@gmail.com>
2023-05-28 21:54:04 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { StyleSheet, Text, View,Dimensions } from 'react-native'
+
+import HomeScreen2 from './src/screens/HomeScreen2'
+import HomeScreen from './src/screens/HomeScreen'
+import RequestScreen from './src/screens/RequestScreen'
+import DestinationScreen from './src/screens/DestinationScreen'
+ import { OriginContextProvider,DestinationContextProvider } from './src/contexts/contexts'
+ import RoootNavigator from './src/navigations/RootNavigator'
+
+
+const App = () => {
+  return (
+    //<View>
+    //  <DestinationScreen/>
+    //</View>
+    //<RoootNavigator />
+   <DestinationContextProvider>
+     <OriginContextProvider>
+         <RoootNavigator />
+    </OriginContextProvider>
+    </DestinationContextProvider>
+   
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1
+  }
+
+})
